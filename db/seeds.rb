ccea116514a9cd66b70f9a1ab7a0049599a72c31
Isaiah Boren <MistaZayuh@gmail.com>
2019-09-06 16:48:22 -0600

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,28 @@ User.create(
   password_confirmation: "password"
 )
 10.times do
-Trip.create(
-  name: Faker::Address.country,
-  user_id: 1,
-  start_date: Faker::Date.forward(days: 23),
-  end_date: Faker::Date.forward(days: 37)
-)
+  trip = Trip.create(
+    name: Faker::Address.country,
+    user_id: 1,
+    start_date: Faker::Date.forward(days: 23),
+    end_date: Faker::Date.forward(days: 37)
+  )
+  5.times do
+    location = Location.create(
+      name: Faker::Address.city,
+      days: rand(1...7),
+      trip_id: trip.id
+    )
+    5.times do 
+      Address.create(
+      street: Faker::Address.street_name,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      zip: Faker::Address.zip,
+      location_id: location.id
+    )
+    end
+  end
 end
-30.times do
-Location.create(
-  name: Faker::Address.city,
-  days: rand(1...7),
-  trip_id: rand(1...@trip)
-)
-end
-50.times do 
-  Address.create(
-  street: Faker::Address.street_name,
-  city: Faker::Address.city,
-  state: Faker::Address.state,
-  zip: Faker::Address.zip,
-  location_id: rand(1...@location)
-)
-end
-
 
 puts "Data Seeded"
